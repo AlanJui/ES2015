27b80de14695a553c56d265f0ec4e6a5c4c555d5
Alan Jui <alanjui.1960@gmail.com>
2015-12-26 16:48:20 +0800

    確定 ES2015 在 Class 及 static method 的用法

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,26 @@
-console.log('Start your engine......');
+// console.log('Go Go Go!!!');
+
+class Post  {
+
+	static getFromServer(path) {
+		return `response: ${path}`;
+	}
+
+  static findAll() {
+  	console.log('find all posts...');
+  	let list = this.getFromServer('posts');
+  	console.log(`Fetch data: ${list}`);
+  }
+}
+
+let UI = {
+	renderPosts() {
+		console.log('Render posts.....');
+	}	
+};
+
+
+Post.findAll();
+
+// Post.findAll()
+// 	.then(UI.renderPosts);
